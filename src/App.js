0bb2d264c7ef73a3b67ab8bf1268ba05e0e0cd87
max <maxkuhn@gmail.com>
2023-03-21 10:07:59 -0700

    Program now generates good blogs, tweets, and LI/FB posts

--- src/App.js
+++ src/App.js
@@ -1,29 +1,68 @@
-import './App.css';
-import React from 'react';
-import { useState } from 'react';
+import "./App.css";
+import React from "react";
+import { useState, useEffect } from "react";
+import { Configuration, OpenAIApi } from "openai";
+import Blog from "./components/Blog";
+import { Si1001Tracklists } from "react-icons/si";
 
+const configuration = new Configuration({
+    apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+});
 
+const openai = new OpenAIApi(configuration);
 
 function App() {
-  const fruit = process.env.REACT_APP_GONKY_FRUIT;
-  console.log(" env is ", process.env);
-  console.log(" fruit is ", fruit);
+    const [slogan, setSlogan] = useState(
+        "Unleash the Ape-Peal: Make Marketing Magic with our Monkey Mastermind!"
+    );
+    const [showBlog, setShowBlog] = useState(false);
+    const [showRap, setShowRap] = useState(false);
+    const [prompt, setPrompt] = useState("ways to learn history");
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={'./gonky-marketing-m.png'} className="App-logo" alt="logo" />
-        <p>
-          My name is Gonky and I'm here to provide<br />
-          some funky marketing content, server-side!<br />
-          I'll write tweets and blogs to make your app explode<br />
-          and give your startup team more time to code!<br />
-        </p>
-        <p>Hey funky folks! Did you know my favourite fruit is {fruit}?</p>
+    const handleText = (event) => {
+        event.preventDefault();
+        let text = event.target.value;
+        setPrompt(text);
+        console.log("prompt is now", prompt);
+    };
 
-      </header>
-    </div>
-  );
+    const viewBlog = () => {
+        console.log("showing blog!");
+        setShowBlog(true);
+    };
+    const viewRap = () => {
+        setShowRap(!showRap);
+    };
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img
+                    src={"./gonky-marketing-m.png"}
+                    className="App-logo"
+                    alt="logo"
+                />
+                <div id="rapbutton" onClick={viewRap}>
+                    <Si1001Tracklists />
+                </div>
+                {showRap && (
+                    <p>
+                        My name is Gonky and I'm here to provide
+                        <br />
+                        some funky marketing content, server-side!
+                        <br />
+                        I'll write tweets and blogs to make your app explode
+                        <br />
+                        and give your startup team more time to code!
+                        <br />
+                    </p>
+                )}
+                <input onChange={handleText} />
+                <button onClick={viewBlog}>Make Blog</button>
+                {showBlog && <Blog prompt={prompt} />}
+            </header>
+        </div>
+    );
 }
 
 export default App;
